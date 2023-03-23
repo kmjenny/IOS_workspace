ce93d5947c1855d507f1df2c6bd6bbe0db2fae2b
jenny <88226911+kmjenny@users.noreply.github.com>
2023-03-23 18:28:25 +0900

    create new project

---
+++ OddEvenGame/OddEvenGame/OddEvenGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddEvenGameApp.swift
+//  OddEvenGame
+//
+//  Created by Jenny Kim on 2023/03/23.
+//
+
+import SwiftUI
+
+@main
+struct OddEvenGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
